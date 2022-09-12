7a54a0257ad9f3aea5bff1f2e60ded08cd3bb707
MyLhassane <mylhassanedeveloper@gmail.com>
2022-09-12 18:52:24 +0100

    Created Hero Component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Hero from '../components/Hero'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <h1 className='text-3xl text-center text-emerald-900 font-bold underline'>Home or not</h1>
+      <Hero />
     </div>
   )
 }
