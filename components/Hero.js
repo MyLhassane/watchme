e475b1720787c2cff7f852d43c4a423ad2a6ec3f
MyLhassane <mylhassanedeveloper@gmail.com>
2022-09-12 18:58:21 +0100

    Added Link (Button)

--- components/Hero.js
+++ components/Hero.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Hero = () => {
   return (
@@ -6,6 +7,10 @@ const Hero = () => {
       <div className="w-60 mx-auto">
         <Image src={"/home_cinema.png"}  width={200} height={200} layout="responsive" />
       </div>
+      <h1 className='text-2xl text-gray-700 uppercase font-bold'>Welcome to WatchMe</h1>
+      <p className='text-gray-500'>Produce FILM feature, TELEVISION and GAME.</p>
+      <Link href="/">
+      <button className='bg-gray-700 text-white py-3 px-6 rounded text-sm mt-4'>CONTACT US</button></Link>
     </div>
   )
 }
