37579e733c9227475d8e1b0f8a60f052f4cfd942
MyLhassane <mylhassanedeveloper@gmail.com>
2022-09-12 19:03:06 +0100

    Create Contact Page

--- components/Hero.js
+++ components/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
       </div>
       <h1 className='text-2xl text-gray-700 uppercase font-bold'>Welcome to WatchMe</h1>
       <p className='text-gray-500'>Produce FILM feature, TELEVISION and GAME.</p>
-      <Link href="/">
+      <Link href="/contact">
       <button className='bg-gray-700 text-white py-3 px-6 rounded text-sm mt-4'>CONTACT US</button></Link>
     </div>
   )
