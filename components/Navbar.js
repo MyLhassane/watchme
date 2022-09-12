9a41c3366bc5e7c29b86b568ba3898c03d567c96
MyLhassane <mylhassanedeveloper@gmail.com>
2022-09-12 19:53:54 +0100

    Added Nenu font

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='bg-gray-700'>
-        <div className='font-bold text-neutral-100 p-4 max-w-7xl mx-auto container tracking-widest'>
+        <div className='font-bold text-neutral-100 p-4 max-w-7xl mx-auto container tracking-widest font-neue'>
             <Link href="/">
                 <a className='text-base md:text-2xl'>Watch
                     <span className='text-red-600'>Me</span>
