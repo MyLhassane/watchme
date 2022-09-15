09c5349241da061f1e4eebfc4fa1199adf9d319d
mylhassanedeveloper <mylhassanedeveloper@gmail.com>
2022-09-15 19:21:50 +0100

    Sending some data form getStaticProps to home component

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,21 @@ import Image from 'next/image'
 import Hero from '../components/Hero'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ movies }) {
+  console.log(movies)
   return (
     <div>
       <Hero />
     </div>
   )
 }
+
+export async function getStaticProps() {
+  const movies = [{ name: 'hello' }];
+
+  return {
+    props: { movies }
+  }
+}
+
+// https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
